0ad93b62e0d5f5c5ea1b192486a5a5a12af50747
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-16 09:26:42 +0300

    state

--- lib/bloc/todo_state.dart
+++ lib/bloc/todo_state.dart
@@ -1,10 +1,32 @@
 part of 'todo_bloc.dart';
 
-sealed class TodoState extends Equatable {
+abstract class TodoState extends Equatable {
   const TodoState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class TodoInitial extends TodoState {}
+class TodoInitial extends TodoState {}
+
+class TodoLoading extends TodoState {}
+
+class TodoLoaded extends TodoState {
+  final List<String> todoList;
+  const TodoLoaded(this.todoList);
+
+  @override
+  List<Object> get props => [todoList];
+}
+
+class FavoriteAdded extends TodoState {
+  final List<String> favoriteList;
+  const FavoriteAdded(this.favoriteList);
+
+  @override
+  List<Object> get props => [favoriteList];
+}
+
+class FavoriteRemoved extends TodoState {}
+
+class FavoriteError extends TodoState {}
