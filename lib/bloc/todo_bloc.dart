a904e22096efd4cfb1c1eacf9bfb8627605250b1
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-16 10:33:07 +0300

    bloc

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -1,13 +1,83 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hive_x_hydrated_bloc_x_bloc_observer/repository/hive_repo.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
-  TodoBloc() : super(TodoInitial()) {
-    on<TodoEvent>((event, emit) {
-      // TODO: implement event handler
+  final HiveRepo hiveRepository;
+  TodoBloc(this.hiveRepository) : super(TodoInitial()) {
+    on<FetchTodo>(
+      (event, emit) {
+        emit(TodoLoading());
+        try {
+          final todoList = hiveRepository.getTodo();
+          emit(TodoLoaded(todoList));
+        } catch (e) {
+          log(e.toString());
+        }
+      },
+    );
+    on<AddTodo>((event, emit) {
+      emit(TodoLoading());
+      try {
+        hiveRepository.addTodo(event.task);
+        final currentTodos = hiveRepository.getTodo();
+        emit(TodoLoaded(currentTodos));
+      } catch (e) {
+        log(e.toString());
+      }
     });
+    on<RemoveTodo>(
+      (event, emit) {
+        emit(TodoLoading());
+        try {
+          hiveRepository.removeTodo(event.task);
+          final currentTodos = hiveRepository.getTodo();
+          emit(TodoLoaded(currentTodos));
+        } catch (e) {
+          log(e.toString());
+        }
+      },
+    );
+    on<FetchFavorites>(
+      (event, emit) {
+        emit(TodoLoading());
+        try {
+          final favoriteList = hiveRepository.getFavorites();
+          emit(TodoLoaded(favoriteList));
+        } catch (e) {
+          log(e.toString());
+        }
+      },
+    );
+
+    on<AddTodoFavorite>(
+      (event, emit) {
+        emit(TodoLoading());
+        try {
+          hiveRepository.addToFavorite(event.task);
+          final currentTodos = hiveRepository.getTodo();
+          emit(TodoLoaded(currentTodos));
+        } catch (e) {
+          log(e.toString());
+        }
+      },
+    );
+    on<RemoveTodoFavorite>(
+      (event, emit) {
+        emit(TodoLoading());
+        try {
+          hiveRepository.removeFromFavorites(event.task);
+          final currentTodos = hiveRepository.getTodo();
+          emit(TodoLoaded(currentTodos));
+        } catch (e) {
+          log(e.toString());
+        }
+      },
+    );
   }
 }
--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -7,6 +7,14 @@ abstract class TodoEvent extends Equatable {
   List<Object> get props => [];
 }
 
+class FetchTodo extends TodoEvent {
+  final List<String> todos;
+  const FetchTodo(this.todos);
+
+  @override
+  List<Object> get props => [todos];
+}
+
 class AddTodo extends TodoEvent {
   final String task;
   const AddTodo(this.task);
@@ -15,8 +23,34 @@ class AddTodo extends TodoEvent {
   List<Object> get props => [task];
 }
 
-class RemoveTodo extends TodoEvent {}
+class RemoveTodo extends TodoEvent {
+  final String task;
+  const RemoveTodo(this.task);
 
-class AddTodoFavorite extends TodoEvent {}
+  @override
+  List<Object> get props => [task];
+}
 
-class RemoveTodoFavorite extends TodoEvent {}
+class FetchFavorites extends TodoEvent {
+  final List<String> todos;
+  const FetchFavorites(this.todos);
+
+  @override
+  List<Object> get props => [todos];
+}
+
+class AddTodoFavorite extends TodoEvent {
+  final String task;
+  const AddTodoFavorite(this.task);
+
+  @override
+  List<Object> get props => [task];
+}
+
+class RemoveTodoFavorite extends TodoEvent {
+  final String task;
+  const RemoveTodoFavorite(this.task);
+
+  @override
+  List<Object> get props => [task];
+}
